50a3f490a347b0726f06675780f6b5133f0ac7d0
TOPILITA <61023571+TOPILITA@users.noreply.github.com>
2022-12-05 20:14:58 +0200

    Add files via upload

---
+++ Lab4/topi-lab4/LabsLibrary/Lab2.cs
@@ -0,0 +1,51 @@
+﻿namespace LabsLibrary
+{
+	public static class Lab2
+	{
+		private static int[] a;
+		private static int[,] d;
+		private static int n, k;
+
+		public static string Execute(string pathInpFile = "INPUT.TXT")
+		{
+			var inputNumbers = File.ReadLines(pathInpFile).First().Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => Convert.ToInt32(n)).ToList();
+
+			if (inputNumbers[0] < 1 || inputNumbers[0] > 180)
+			{
+				return "Out of range exception!";
+			}
+			else
+			{
+				n = inputNumbers.First();
+				k = inputNumbers.Last();
+				a = inputNumbers.Skip(1).SkipLast(1).ToArray();
+				d = new int[k + 1, n + 1];
+				return GetMaxNumberOfCoins().ToString();
+			}
+		}
+
+		private static int GetMaxNumberOfCoins()
+		{
+			return f(k, 0);
+		}
+
+		private static int f(int k, int i)
+		{
+			if (d[k, i] == 0)
+			{
+				d[k, i] = l(Math.Min(k, n - i), 0, i);
+			}
+			return d[k, i];
+		}
+
+		private static int l(int j, int r, int i)
+		{
+			return j <= 0 ? r : l(j - 1, Math.Max(r, s(i) - f(j, i + j)), i);
+		}
+
+		private static int s(int i)
+		{
+			return i >= n ? 0 : a[i] + s(i + 1);
+		}
+	}
+}
---
+++ Lab4/topi-lab4/topi-lab4/Program.cs
@@ -0,0 +1,196 @@
+﻿using LabsLibrary;
+using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+[Command(Name = "lab4", Description = "lab4"),
+Subcommand(typeof(Run), typeof(Version), typeof(SetPath))]
+class Lab4
+{
+	private const string LAB_PATH = "LAB_PATH";
+	public static void Main(string[] args) => CommandLineApplication.Execute<Lab4>(args);
+	private int OnExecute(CommandLineApplication app, IConsole console)
+	{
+		console.WriteLine("Commands:");
+
+		console.WriteLine($"Current platform is: " + Environment.OSVersion.Platform);
+		app.ShowHelp();
+		return 1;
+	}
+
+	[Command("version", Description = "Version")]
+	private class Version
+	{
+		private int OnExecute(IConsole console)
+		{
+			var actualPath = SetPath.GetLabPathEnv();
+			Console.WriteLine("Tamara Syrota, ips-43");
+			console.WriteLine($"Version: 1.0.7");
+			console.WriteLine($"Variable LAB_PATH: {(string.IsNullOrWhiteSpace(actualPath) ? "not set" : actualPath)}");
+			return 1;
+		}
+	}
+
+	[Command("run", Description = "run"), Subcommand(typeof(Lab1Presenter)), Subcommand(typeof(Lab2Presenter))]
+	private class Run
+	{
+		private int OnExecute(IConsole console)
+		{
+			console.Error.WriteLine("You didn't choose the lab to execute");
+			return 1;
+		}
+
+		private abstract class LabsLibrary
+		{
+			[Option("--input -i", Description = "input")]
+			public string Input { get; } = null!;
+
+			[Option("--output -o", Description = "output")]
+			public string Output { get; } = null!;
+
+			protected string GetInputPath()
+			{
+				if (File.Exists(Input))
+				{
+					return Input;
+				}
+
+				var path = SetPath.GetLabPathEnv();
+
+				if (string.IsNullOrWhiteSpace(path))
+				{
+					path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+				}
+				path = Path.Combine(path, "INPUT.TXT");
+
+				if (File.Exists(path))
+				{
+					return path;
+				}
+
+				return string.Empty;
+			}
+			protected string GetOutputPath()
+			{
+				if (!string.IsNullOrWhiteSpace(Output))
+				{
+					return Output;
+				}
+
+				var path = SetPath.GetLabPathEnv();
+
+				if (string.IsNullOrWhiteSpace(path))
+				{
+					path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+				}
+
+				if (Directory.Exists(path))
+				{
+					return Path.Combine(path, "OUTPUT.TXT");
+				}
+
+				return "OUTPUT.TXT";
+			}
+
+			protected string ReadInputFile()
+			{
+				string inputPath = GetInputPath();
+
+				if (string.IsNullOrWhiteSpace(inputPath))
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine($"not found");
+					Console.ForegroundColor = ConsoleColor.White;
+					return string.Empty;
+				}
+
+				Console.WriteLine($"input - {inputPath}");
+
+				return File.ReadAllText(inputPath);
+			}
+			protected void WriteOuputFile(string outputData)
+			{
+				string outputPath = GetOutputPath();
+
+				Console.WriteLine($"output - {outputPath}");
+
+				File.WriteAllText(outputPath, outputData);
+			}
+
+			protected virtual int OnExecute(IConsole console)
+			{
+				console.WriteLine("input - " + Input + "output - " + Output);
+				return 1;
+			}
+		}
+
+		[Command("lab1", Description = "lab1")]
+		private class Lab1Presenter : LabsLibrary
+		{
+			protected override int OnExecute(IConsole console)
+			{
+				string inputData = ReadInputFile();
+				if (string.IsNullOrWhiteSpace(inputData))
+				{
+					return -1;
+				}
+
+				var result = Lab1.Execute(Input);
+				console.WriteLine($"Output: " + result);
+
+				WriteOuputFile(result.ToString());
+				return 1;
+			}
+		}
+
+		[Command("lab2", Description = "lab2")]
+		private class Lab2Presenter : LabsLibrary
+		{
+			protected override int OnExecute(IConsole console)
+			{
+				string inputData = ReadInputFile();
+				if (string.IsNullOrWhiteSpace(inputData))
+				{
+					return -1;
+				}
+
+				var result = Lab2.Execute(Input);
+				console.WriteLine($"Output: " + result);
+
+				WriteOuputFile(result.ToString());
+				return 1;
+			}
+		}
+	}
+
+	[Command("set-path", Description = "Set LAB_PATH")]
+	private class SetPath
+	{
+		[Option("--path -p", Description = "set path")]
+		[Required(ErrorMessage = "set path")]
+		public string Path { get; } = null!;
+		private int OnExecute(IConsole console)
+		{
+			Environment.SetEnvironmentVariable(LAB_PATH, Path);
+
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				File.WriteAllText(".env", $"{LAB_PATH}={Path}");
+			}
+
+			return 1;
+		}
+		public static string GetLabPathEnv()
+		{
+			var path = Environment.GetEnvironmentVariable(LAB_PATH);
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				var text = File.ReadAllText(".env");
+				var keyValue = text.Split('=');
+				if (keyValue.Length == 2)
+					path = keyValue[1];
+			}
+			return path ?? "";
+		}
+	}
+}
